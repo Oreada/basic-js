b821058155c8cdf585e3dd03cfc02e516b7d4fc5
Olga Antushevich <antushevich@gmail.com>
2022-04-28 22:48:46 +0300

    feat: implement the countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function countCats(backyard) {
+  let cnt = 0;
+  for (let inner of backyard) {
+    inner.forEach(element => {
+      if (element == '^^') {
+        cnt++;
+      };
+    });
+  }
+  return cnt;
+};
 
 module.exports = {
   countCats
