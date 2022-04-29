ef99b97273da0affa058d725ba3070ce4c0adb1e
Olga Antushevich <antushevich@gmail.com>
2022-04-29 22:47:48 +0300

    feat: implement the encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let prev = '', cnt = 0, result = '';
+  for (let c of str) {
+    if (prev == c) {
+      cnt++;
+    } else {
+      if (prev != '') {
+        result += (cnt > 1 ? cnt : "") + prev;
+      }
+      cnt = 1;
+      prev = c;
+    }
+  }
+  if (prev != '') {
+    result += (cnt > 1 ? cnt : "") + prev;
+  }
+  return result;
 }
 
 module.exports = {
