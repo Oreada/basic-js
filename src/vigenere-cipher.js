d45f5a8064c0701f9fe811a54cd99cf5a5518593
Olga Antushevich <antushevich@gmail.com>
2022-04-30 03:02:24 +0300

    feat: implement the vigenereCipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+
+    this.mode = typeof mode == 'undefined' ? true : mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(s, key) {
+    if (typeof s == 'undefined' || typeof key == 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+    s = s.toUpperCase().split('');
+    key = key.toUpperCase();
+    let shift = [];
+    for (let k of key) {
+      shift.push(k.charCodeAt(0) - 'A'.charCodeAt(0));
+    }
+    let spaceSkip = 0;
+    s.forEach((element, index) => {
+      if (element.charCodeAt(0) >= 'A'.charCodeAt(0) && element.charCodeAt(0) <= 'Z'.charCodeAt(0)) {
+        let curShift = shift[(index - spaceSkip) % shift.length];
+        if (element.charCodeAt(0) - 'A'.charCodeAt(0) + curShift >= 26) {
+          curShift = curShift - 26;
+        }
+        s[index] = String.fromCharCode(element.charCodeAt(0) + curShift);
+      } else {
+        spaceSkip++;
+      }
+    });
+    if (!this.mode) {
+      s = s.reverse();
+    }
+    return s.join('');
+  }
+  decrypt(s, key) {
+    if (typeof s == 'undefined' || typeof key == 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+    s = s.toUpperCase().split('');
+    key = key.toUpperCase();
+    let shift = [];
+    for (let k of key) {
+      shift.push('A'.charCodeAt(0) - k.charCodeAt(0));
+    }
+    let spaceSkip = 0;
+    s.forEach((element, index) => {
+      if (element.charCodeAt(0) >= 'A'.charCodeAt(0) && element.charCodeAt(0) <= 'Z'.charCodeAt(0)) {
+        let curShift = shift[(index - spaceSkip) % shift.length];
+        if (element.charCodeAt(0) - 'A'.charCodeAt(0) + curShift < 0) {
+          curShift = curShift + 26;
+        }
+        s[index] = String.fromCharCode(element.charCodeAt(0) + curShift);
+      } else {
+        spaceSkip++;
+      }
+    });
+    if (!this.mode) {
+      s = s.reverse();
+    }
+    return s.join('');
   }
 }
 
